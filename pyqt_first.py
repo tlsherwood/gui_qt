f6f890387bdfd799924000ff44eabbbccea96c24
tlsherwood <toddsherwood@outlook.com>
2019-01-07 09:38:45 -0500

    Delete of old code for cleanup

--- pyqt_first.py
+++ pyqt_first.py
@@ -28,31 +28,3 @@ if __name__ == '__main__':
     window.show()
     sys.exit(app.exec_())
 
-# from PyQt5 import QtCore, QtGui, uic
-#
-# qtCreatorFile = 'tax_calculator.ui' # Enter file here.
-#
-# Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
-#
-# # class MyApp(QtGui.QMainWindow, Ui_MainWindow):
-# class MyApp(QtGui.QWindow, Ui_MainWindow):
-#     def __init__(self):
-#         # QtGui.QMainWindow.__init__(self)
-#         QtGui.QWindow.__init__(self)
-#         Ui_MainWindow.__init__(self)
-#         self.setupUi(self)
-#         self.calc_tax_button.clicked.connect(self.CalculateTax)
-#
-#     def CalculateTax(self):
-#         price = int(self.price_box.toPlainText())
-#         tax = (self.tax_rate.value())
-#         total_price = price + ((tax / 100) * price)
-#         total_price_string = "The total price with tax is: " + str(total_price)
-#         self.results_window.setText(total_price_string)
-#
-# if __name__ == "__main__":
-#     # app = QtGui.QApplication(sys.argv)
-#     app = QtGui.QGuiApplication(sys.argv)
-#     window = MyApp()
-#     window.show()
-#     sys.exit(app.exec_())
