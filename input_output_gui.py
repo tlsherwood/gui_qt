7b604f4de479758ce50a1e6ad4664f2b45d0bfd3
tlsherwood <toddsherwood@outlook.com>
2019-01-10 18:25:41 -0500

    More attempts
    still can't get the Browse button to act as a true Browse button - it seems to me like my tkinter approach is much simpler for my purposes anyway.

--- input_output_gui.py
+++ input_output_gui.py
@@ -1,32 +1,51 @@
-# import sys
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'input_output_gui.ui'
+#
+# Created by: PyQt5 UI code generator 5.6
+#
+# WARNING! All changes made in this file will be lost!
+
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import QObject, pyqtSlot
-from PyQt5.QtWidgets import QMainWindow, QApplication
-from PyQt5 import uic
 
-Ui_MainWindow, QtBaseClass = uic.loadUiType('input_output_gui.ui')
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(870, 568)
+        self.frame = QtWidgets.QFrame(Dialog)
+        self.frame.setGeometry(QtCore.QRect(10, 90, 721, 81))
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
+        self.widget = QtWidgets.QWidget(self.frame)
+        self.widget.setGeometry(QtCore.QRect(10, 30, 711, 25))
+        self.widget.setObjectName("widget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.widget)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.label = QtWidgets.QLabel(self.widget)
+        self.label.setObjectName("label")
+        self.horizontalLayout.addWidget(self.label)
+        self.lineEdit = QtWidgets.QLineEdit(self.widget)
+        self.lineEdit.setObjectName("lineEdit")
+        self.horizontalLayout.addWidget(self.lineEdit)
+        self.pushButton = QtWidgets.QPushButton(self.widget)
+        self.pushButton.setObjectName("pushButton")
+        self.horizontalLayout.addWidget(self.pushButton)
 
-class IO_Dialogue(QMainWindow):
-    def __init__(self):
-        super(IO_Dialogue, self).__init__()
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-
-        # self.statusbar.setObjectName('statusbar')
-        # window.setStatusBar(self.statusbar)
-
-        self.retranslateUi(window)
-        # self.retranslateUi(self)
-
+        self.retranslateUi(Dialog)
+        # self.pushButton.clicked.connect(Dialog.browseSlot)
+        # self.lineEdit.returnPressed.connect(Dialog.returnPressedSlot)
         self.pushButton.clicked.connect(self.browseSlot)
         self.lineEdit.returnPressed.connect(self.returnPressedSlot)
-        QtCore.QMetaObject.connectSlotsByName(window)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
 
-    def retranslateUi(self, window):
+    def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
-        window.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.label.setText(_translate("MainWindow", "File Name"))
-        self.pushButton.setText(_translate("MainWindow", "Browse"))
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.label.setText(_translate("Dialog", "File Name"))
+        self.pushButton.setText(_translate("Dialog", "Browse"))
 
     @pyqtSlot()
     def returnPressedSlot(self):
@@ -36,80 +55,13 @@ class IO_Dialogue(QMainWindow):
     def browseSlot(self):
         pass
 
-if __name__ == '__main__':
-    import sys
-    app = QApplication(sys.argv)
-    window = IO_Dialogue()
-    # window = QtWidgets.QMainWindow()
-    # ui = Ui_MainWindow()
-    # ui.setupUi(MainWindow)
-    window.show()
-    sys.exit(app.exec_())
 
-
-
-class Ui_MainWindow(QObject):
-    def __init__(self):
-        super(Ui_MainWindow, self).__init__()
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-# class Ui_MainWindow(QMainWindow):
-    # self.statusbar.setObjectName('statusbar')
-    # MainWindow.setStatusBar(self.statusbar)
-
-    self.retranslateUi(MainWindow)
-
-    self.pushButton.clicked.connect(self.browseSlot)
-    self.lineEdit.returnPressed.connect(self.returnPressedSlot)
-    QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.label.setText(_translate("MainWindow", "File Name"))
-        self.pushButton.setText(_translate("MainWindow", "Browse"))
-
-    @pyqtSlot()
-    def returnPressedSlot(self):
-        pass
-
-    @pyqtSlot()
-    def browseSlot(self):
-        pass
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(ap.exec_())
-    # ui.setupUi(Ui_MainWindow)
-
-
-# class MyApp(QMainWindow):
-#     def __init__(self):
-#         super(MyApp, self).__init__()
-#         self.ui = Ui_MainWindow()
-#         self.ui.setupUi(self)
-#         self.ui.input_file_browse_button.clicked.connect(self.BrowseInputFile)
-#         self.ui.ok_cancel_buttons.clicked.connect(self.RunScript)
-#
-#     def BrowseInputFile(self):
-#         price = float(self.ui.price_box.toPlainText())
-#         tax = (self.ui.tax_rate.value())
-#         tax_paid = (tax / 100) * price
-#         total_price = price + tax_paid
-#         # print (total_price)
-#         tax_paid_string = 'The tax paid is: ' + str(tax_paid)
-#         total_price_string = 'The total price with tax is: ' + str(total_price)
-#         self.ui.results_window_tax.setText(tax_paid_string)
-#         self.ui.results_window_total.setText(total_price_string)
-#
-# if __name__ == '__main__':
-#     app = QApplication(sys.argv)
-#     window = MyApp()
-#     window.show()
-#     sys.exit(app.exec_())
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
 
---
+++ mainwindow.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'mainwindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.6
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import QObject, pyqtSlot
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(854, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.widget = QtWidgets.QWidget(self.centralwidget)
+        self.widget.setGeometry(QtCore.QRect(30, 70, 721, 25))
+        self.widget.setObjectName("widget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.widget)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.label = QtWidgets.QLabel(self.widget)
+        self.label.setObjectName("label")
+        self.horizontalLayout.addWidget(self.label)
+        self.lineEdit = QtWidgets.QLineEdit(self.widget)
+        self.lineEdit.setObjectName("lineEdit")
+        self.horizontalLayout.addWidget(self.lineEdit)
+        self.pushButton = QtWidgets.QPushButton(self.widget)
+        self.pushButton.setObjectName("pushButton")
+        self.horizontalLayout.addWidget(self.pushButton)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 854, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        # self.pushButton.clicked.connect(MainWindow.browseSlot)
+        # self.lineEdit.returnPressed.connect(MainWindow.returnPressedSlot)
+        self.pushButton.clicked.connect(self.browseSlot)
+        self.lineEdit.returnPressed.connect(self.returnPressedSlot)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Select File"))
+        self.label.setText(_translate("MainWindow", "File Name"))
+        self.pushButton.setText(_translate("MainWindow", "Browse"))
+
+    @pyqtSlot()
+    def returnPressedSlot(self):
+        pass
+
+    @pyqtSlot()
+    def writeDocSlot(self):
+        pass
+
+    @pyqtSlot()
+    def browseSlot(self):
+        self.
+        pass
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
